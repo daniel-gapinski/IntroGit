e652a50d5d2c71bf8fed85c5b44ea93ad8677641
Daniel Augusto Gapinski <daniel.gapinski@hotmail.com>
2024-06-20 08:58:25 -0300

    Comit para repositorio remoto

--- Aula01_IniciandoGit/index.php
+++ Aula01_IniciandoGit/index.php
@@ -6,6 +6,23 @@
     <title>Git</title>
 </head>
 <body>
+
+<!--
+    Comandos importantes para o GIT
+
+    -> git status: mostra o arquivo alterado  
+    -> git diff: mostra o que foi alterado dentro do arquivo
+    -> git commit: (git commit -m "Nome do repositório") para enviar para o repositório 
+    -> git add -A: para adicionar todos os novos arquivos par ao  commit
+    -> git branch: vai mostrar todos os branchs
+    -> git branch "nome_do_branch": para criar um novo branch
+    -> git checkout "nome_do_branch" para criar e ir para o branch que foi criado
+    -> Reverter repositório: voltar o repositório para versão anterior: git reset --hard (código do commit)
+    -> git remote rm origin: remover do repositório de origem para enviar para outro
+    -> ver em qual repositório está o projeto: git remote -v
+    -> Conectar no git remoto: git remote add origin (URL do git hub onde é para ficar o projeto)
+    -> puxar do repositório remoto para o local: git pull origin master
+-->
     
 </body>
 </html>
